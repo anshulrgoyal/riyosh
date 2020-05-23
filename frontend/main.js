6b82b98923b24dad3be76e00b86c3fde2add7453
anshul goyal <agoyal@localhost.localdomain>
2020-05-23 15:02:59 -0400

    Added pakcage.json

--- frontend/main.js
+++ frontend/main.js
@@ -8,7 +8,7 @@ window.onload = () => {
 
 document.getElementById("submit").addEventListener("click", () => {
   const valeu = document.getElementById("input").value;
-  fetch("http://localhost:3000/", {
+  fetch("http://localhost:3000/c", {
     method: "post",
     headers: { "content-type": "application/json" },
     body: JSON.stringify({ code: editor.getValue(), input: valeu })
